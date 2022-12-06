7d38ec51c4f360916cb51271addcf750ad179735
Ilie1985 <ilie.marian32@yahoo.com>
2022-12-06 16:47:43 +0000

    display the photos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
-const clientID=`?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
@@ -19,7 +19,7 @@ function App() {
       const response = await fetch(url);
       const data = await response.json();
 
-      console.log(data);
+      setPhotos(data);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -31,7 +31,32 @@ function App() {
     fetchImages();
   }, []);
 
-  return <h2>stock photos starter</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("gello");
+  };
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="search" className="form-input" />
+          <button className="submit-btn" type="submit">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
 }
 
 export default App;
